9f942d7074974fa504442d5e5c7fea6df54240c7
dcdnce <pforesti@student.42nice.fr>
2023-12-12 17:11:24 +0100

    from server to browser

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 import { createBoard, playMove } from "./connect4.js";
 
 window.addEventListener("DOMContentLoaded", () => {
-	// Initialize the UI.
+	// Initialize the UI
 	const board = document.querySelector(".board");
 	createBoard(board);
 
-	// Open the WebSocket connection and register event handlers.
+	// Open the WebSocket connection 
 	const websocket = new WebSocket("ws://localhost:8001/");
+	// Register event handlers
 	sendMoves(board, websocket);
+	receivesMoves(board, websocket);
 });
 
 function sendMoves(board, websocket) {
@@ -21,5 +23,31 @@ function sendMoves(board, websocket) {
 			column: parseInt(column, 10),
 		};
 		websocket.send(JSON.stringify(newEvent));
+	});
+}
+
+function showMessage(message) {
+	window.setTimeout(() => window.alert(message), 50);
+}
+
+function receivesMoves(board, websocket) {
+	websocket.addEventListener("message", ({ data }) => {
+		const event = JSON.parse(data);
+		switch (event.type) {
+			case "play":
+				// Update the UI with the move.
+				playMove(board, event.player, event.column, event.row);
+				break;
+		 	case "win":
+				showMessage(`Player ${event.player} wins!`);
+				// close the WebSocket connection.
+				websocket.close(1000);
+			break;
+				case "error":
+				showMessage(event.message);
+				break;
+		  default:
+				throw new Error(`Unsupported event type: ${event.type}.`);
+    }
 	});
 }
